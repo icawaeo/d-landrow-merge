a4eed636b5bcd9ca43b23c3a9abfdd05e9068b0b
icawaeo <veronica.waeo@gmail.com>
2025-08-14 03:21:57 +0800

    change color on proyek baru

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -6,7 +6,7 @@
         <div class="px-4 py-4 border-b border-slate-700">
             <x-dropdown align="left" width="60">
                 <x-slot name="trigger">
-                    <button class="w-full flex items-center justify-center gap-2 px-4 py-2 bg-slate-700 text-white rounded-md hover:bg-slate-600 transition focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-800 focus:ring-white">
+                    <button class="w-full flex items-center justify-center gap-2 px-4 py-2 bg-[#0a4f5e] text-white rounded-md hover:bg-[#083F4B] transition focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-800 focus:ring-white">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/></svg>
                         <span>Proyek Baru</span>
                         {{-- <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" /></svg> --}}
